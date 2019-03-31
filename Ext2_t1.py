6a2aedd2cfd78fc530520b94bac88a2006b7743f
V4riableZ <benben95939@gmail.com>
2019-03-31 18:02:55 +0300

    Updated code to py3

--- Ext2_t1.py
+++ Ext2_t1.py
@@ -2,21 +2,21 @@
 
 from __future__ import division
 
-num1=input(int)('Please enter a number:')
-num2=input(int)('Please enter another number:')
+num1=int(input('Please enter a number:'))
+num2=int(input('Please enter another number:'))
 
 numSum=num1+num2
 numMulti=num1*num2
 numDiv=num1/num2
 
-print("The summary of the numbers is %s"%numSum)
-print("The numbers are multiplied and the result is %s"%numMulti)
-print("The numbers are then divided and the result is %s"%numDiv)
+print("The summary of the numbers is: %s"%numSum)
+print("The numbers are multiplied and the result is: %s"%numMulti)
+print("The numbers are then divided and the result is: %s"%numDiv)
 
 if num1 == num2:
-	print("The numbers are equal %s"%num1,num2)
+	print("The numbers are equal: %s"%num1,num2)
 else:
-	print("The numbers are unequal %s"%num1,num2)
+	print("The numbers are unequal: %s"%num1,num2)
 
 if num1 > num2:
 	print("The first number is bigger than the second number, %s"%num2)
--- Ext2_t2.py
+++ Ext2_t2.py
@@ -5,7 +5,7 @@ opt=[1,2,3]
 str1=input("Enter a word:")
 str2=input("Enter another word:")
 
-num=int(input(("Choose a number:")))
+num=int(input(("Choose a number: %s "%opt)))
 
 if len(str1) > len(str2):
     print(str1)
