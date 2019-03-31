a2afc922a0f5301ff2bf3def3dd6f6dd5f376d53
V4riableZ <benben95939@gmail.com>
2019-03-31 17:55:41 +0300

    Updated some code to py3

--- full_name.py
+++ full_name.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-F_name=input('Enter your first name')
-L_name=input('Enter your last name')
+F_name=input('Enter your first name: ')
+L_name=input('Enter your last name: ')
 
-print("My name is %s" %F_name,L_name)
+print("My name is %s"%F_name,L_name)
--- py_ver.py
+++ py_ver.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import platform
 py_Ver=(platform.python_version())
