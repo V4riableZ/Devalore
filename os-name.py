872d03782a9b944e0b90f99203bf2385c3359891
V4riableZ <benben95939@gmail.com>
2020-01-13 02:32:05 +0200

    Simple script to determine os name and version.

---
+++ os-name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Get OS name and version
+#Importing platform module
+
+import platform
+
+#Declaring variable to place output of platform.linux_distribution
+
+osname = platform.linux_distribution()
+
+#Printing osname variable
+
+print (osname)
